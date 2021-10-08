ea4f42da225e2be29b2115c173d4e9f9f3aaf46e
Marco <sonni46marcosenpai@gmail.com>
2021-10-08 14:39:52 +0200

    Snack 1 finito

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,3 +1,32 @@
 <?php 
-    
+// Creiamo un array contenente le partite di basket di un’ipotetica tappa
+// del calendario.
+    $partite = 
+    [
+        [
+            "squadra di casa" => "Menz",
+            "squadra ospite"  => "Cark",
+            "punti casa" =>  rand(0 , 100),
+            "punti ospite" => rand(0 , 100),
+        ],
+        [
+            "squadra di casa" => "Oland",
+            "squadra ospite"  => "Holid",
+            "punti casa" => rand(0 , 100),
+            "punti ospite" => rand(0 , 100),
+        ],
+        [
+            "squadra di casa" => "Ganz",
+            "squadra ospite"  => "Panet",
+            "punti casa" => rand(0 , 100),
+            "punti ospite" => rand(0 , 100),
+        ],
+    ];
+
+    var_dump($partite);
+
+    for($i = 0; $i < count($partite); $i++) {
+       echo "<h1>" . $partite[$i]["squadra di casa"] . " - " . $partite[$i]["squadra ospite"] . " " . $partite[$i]["punti casa"] . " | " . $partite[$i]["punti ospite"] ."</h1>";
+    };
+
 ?>
